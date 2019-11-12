b37d7c3b569c97aa11a0c1ae4d1b16f23be6a6ad
kaczmarekkacper <kaaczmarekkacper@gmail.com>
2019-11-12 20:53:50 +0100

    Nowa koncepcja

---
+++ src/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QWidget>
+#include <QMainWindow>
+#include <QAbstractSocket>
+class ChatClient;
+class QStandardItemModel;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(MainWindow)
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private:
+    Ui::MainWindow *ui;
+    ChatClient *m_chatClient;
+    QStandardItemModel *m_chatModel;
+    QString m_lastUserName;
+private slots:
+    void attemptConnection();
+    void connectedToServer();
+    void attemptLogin(const QString &userName);
+    void loggedIn();
+    void loginFailed(const QString &reason);
+    void messageReceived(const QString &sender, const QString &text);
+    void sendMessage();
+    void disconnectedFromServer();
+    void userJoined(const QString &username);
+    void userLeft(const QString &username);
+    void error(QAbstractSocket::SocketError socketError);
+};
+#endif // MAINWINDOW_H
